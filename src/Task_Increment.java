ffcdae7858b5293213f9039728801f7acd0a43d1
Siddhesh <gujarsiddhesh603@gmail.com>
2025-01-23 23:37:39 +0530

    Task11

---
+++ src/Task_Increment.java
@@ -0,0 +1,23 @@
+public class Task_Increment {
+    public static void main(String[] args) {
+
+        int a=10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+
+         //A + B +C
+        //A--Exp1-11_->a1-11
+        //B--Exp2-11_->a1-12
+        //C--Exp3-12_->a1-13
+
+        //Exp1 + Exp2 + Exp3=11+11+12=34
+        //a1-13
+
+
+
+    }
+
+
+
+
+}
--- src/TestingAcademy.java
+++ src/TestingAcademy.java
@@ -2,6 +2,6 @@ public class TestingAcademy {
 
     public static void main(String[] args) {
         System.out.println("Excellence");
-        sou
+
     }
 }
