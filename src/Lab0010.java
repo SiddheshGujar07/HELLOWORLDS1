6dec09ffde23a7c2b24161d4a91550c5cfb45c60
Siddhesh <gujarsiddhesh603@gmail.com>
2025-01-25 19:14:39 +0530

    Task12

---
+++ src/Lab0010.java
@@ -0,0 +1,23 @@
+public class Lab0010 {
+
+    public static void main(String[] args) {
+
+        int a=20;
+
+        //A + B +C
+        //A->EXP1-19, a-19
+        //B->EXP1-19, a-20
+        //C->EXP1-20, a-19
+
+         // 19 +19 + 20=58
+
+
+
+
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+
+    }
+
+
+}
