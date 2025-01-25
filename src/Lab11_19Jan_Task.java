b311c8be3ea03745f8d1e7de398f8bb7370c490f
Siddhesh <gujarsiddhesh603@gmail.com>
2025-01-25 19:45:34 +0530

    Task13

---
+++ src/Lab11_19Jan_Task.java
@@ -0,0 +1,25 @@
+public class Lab11_19Jan_Task {
+
+
+//Take a user input - Name, Age and Salary and print them in the end.
+
+    public static void main(String[] args) {
+
+        String name=args[0];
+        System.out.println(name);
+        String age=args[1];
+        int a=Integer.parseInt(age);
+        System.out.println(age);
+        String salarys=args[2];
+        double b=Integer.parseInt(salarys);
+        System.out.println(b);
+
+
+
+
+    }
+
+
+
+
+}
