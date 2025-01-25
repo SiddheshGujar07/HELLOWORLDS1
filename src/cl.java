44b90b91091d99454b4ca6d61218baff1d977bfb
Siddhesh <gujarsiddhesh603@gmail.com>
2025-01-25 20:23:37 +0530

    Task19

---
+++ src/cl.java
@@ -0,0 +1,25 @@
+public class cl {
+
+    //Finding the Largest of Three Numbers a,b,c using the Ternary Operator.
+
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        int c= 30;
+
+        int largest=a>b?(a>c?a:c):(b>c?b:c);
+        System.out.println("Largest Number is "+ largest);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
