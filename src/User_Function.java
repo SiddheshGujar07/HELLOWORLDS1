4a8d1680442b6c7b17dde22d538fb832e3d668f3
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-02 17:12:55 +0530

    Task21

---
+++ src/User_Function.java
@@ -0,0 +1,62 @@
+public class User_Function {
+
+/// /Without Pararmerter and Without Return Type//
+        static void drive()
+        {
+            System.out.println( "Hii Siddhesh");
+        }
+
+        static void test()
+        {
+            System.out.println("Byee Siddhesh");
+
+        }
+
+    /// /Without Pararmerter and With Return Type//
+
+    static String greet_with_hello()
+    {
+
+        return "Hii How are you";
+    }
+
+    static int age()
+    {
+        return 18;
+    }
+    static float area()
+    {
+        return 8.91f;
+    }
+
+
+    public static void main(String[] args) {
+        drive();
+        test();
+
+        String s1=greet_with_hello();
+        System.out.println(s1);
+
+        int a=age();
+        System.out.println(a);
+
+        float b=area();
+        System.out.println(b);
+
+
+
+
+
+
+
+
+
+    }
+
+
+    }
+
+
+
+
+
