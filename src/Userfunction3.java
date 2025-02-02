922ddf0c3c66d18909b33ff2358f8c9681d0c9bd
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-02 23:23:57 +0530

    as

---
+++ src/Userfunction3.java
@@ -0,0 +1,35 @@
+public class Userfunction3 {
+
+/// With Parameter and with return type
+
+    static int sum_of_two_numbers(int a,int b)
+
+    {
+        int c=a+b;
+        return c;
+    }
+
+    static int sum_of_three_numbers(int a,int b,int c)
+
+    {
+
+        int d=a+b+c;
+        return d;
+
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("MB");
+        System.out.println(sum_of_two_numbers(10,20));
+        System.out.println(sum_of_three_numbers(10,20,30));
+        System.out.println("ME");
+
+    }
+
+
+
+
+}
+
+
