457f657197d35e042f560374bd3e60b13602d5de
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-03 23:38:04 +0530

    sdsdsds

---
+++ src/vowels_consonant.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class vowels_consonant {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a character");
+        String input= sc.next()///String into convert
+        char character=input.charAt(0);///then convert into char
+
+        if(character=='A'||character=='B'||character=='C'||character=='E'|| character=='I'||character=='O'||character=='Z')
+
+        {
+            System.out.println("Vowels");
+        }
+
+        else
+        {
+            System.out.println("Consonant");
+        }
+
+
+
+
+    }
+
+
+}
