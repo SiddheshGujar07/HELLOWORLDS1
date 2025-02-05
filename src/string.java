38dacac7a0da41fe557360c20f0e30389dbe06ec
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-06 00:01:15 +0530

    sdsdsdsds

---
+++ src/string.java
@@ -0,0 +1,26 @@
+public class string {
+
+    public static void main(String[] args) {
+
+        String s1="Siddhesh";
+        s1=s1.concat("Gujar");
+        System.out.println(s1);
+
+        String s2="Siddhu";
+        System.out.println(s2.length());
+        System.out.println(s2.concat("Gujar"));
+        System.out.println(s2.toUpperCase());
+        System.out.println(s2.toLowerCase());
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
