b5bd1cbca9d3b5d11d257642ca66103bff42fd68
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-06 00:02:05 +0530

    sdsdsdsdssdsdsdsd

---
+++ src/String_Functions.java
@@ -0,0 +1,20 @@
+public class String_Functions {
+
+    public static void main(String[] args) {
+
+        String s1="I am a excellent boy";
+        String classical=s1.substring(6,12);
+        System.out.println(classical);
+
+        String s2="Siddhesh";
+        System.out.println(s2.concat("Gujar"));
+
+
+
+
+    }
+
+
+
+
+}
