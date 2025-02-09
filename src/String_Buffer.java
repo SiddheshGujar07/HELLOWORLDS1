5a1d0830921d90c498e35a7c70d595beedbd9f57
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-09 23:24:22 +0530

    sdsdsdsdssdsdsdsdaassssddddfdf

---
+++ src/String_Buffer.java
@@ -0,0 +1,27 @@
+public class String_Buffer {
+
+    public static void main(String[] args) {
+
+        StringBuffer stringBuffer=new StringBuffer("Shivani");
+        stringBuffer.append("Gujar");
+        System.out.println(stringBuffer);
+        stringBuffer.reverse();
+        System.out.println(stringBuffer);  ///Only String will be generated.//
+
+        String s1="Shivani";
+        String s2="Supekar";
+        String s3=s1 +s2;
+        System.out.println(s3);///3 String is genertaed in SCP
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
