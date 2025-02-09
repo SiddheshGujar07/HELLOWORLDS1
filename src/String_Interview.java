af5f957da46e93eb057f7f14d729106d06205730
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-09 23:23:32 +0530

    sdsdsdsdssdsdsdsdaassss

--- src/String_Interview.java
+++ src/String_Interview.java
@@ -45,6 +45,24 @@ public class String_Interview {
 
         System.out.println(s2.split("@"));
 
+        //9.Substring.
+
+        String s3="Wikipedia";
+        System.out.println(s3.substring(2,5));
+        //System.out.println(s3.substring(2,12));///-String index out of Bound Exception.
+
+        //10.to lower case and uppdercase.
+        System.out.println(s3.toLowerCase());
+        System.out.println(s3.toUpperCase());
+
+        //11.Starts with and ends with
+        System.out.println(s3.startsWith("W"));
+        System.out.println(s3.endsWith("a"));
+
+        //12.compare to Ignore case()
+
+        System.out.println(s3.compareToIgnoreCase("Wikipedia"));
+
 
 
 
