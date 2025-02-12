d95ed37145c02fcdb5dbafca2e565fcfec7ae93f
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-12 23:04:49 +0530

    sdsdsdsdssdsdsdsdaassssddddfdfAAAAAAssw

---
+++ src/Powernumber.java
@@ -0,0 +1,27 @@
+public class Powernumber {
+
+    public static void main(String[] args) {
+
+        /// 2^5
+        int n=2;
+        int p=5;
+        int product=1;
+
+        for(int i=1;i<=p;i++)
+        {
+            int result=product*2;
+
+            System.out.println(result);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
