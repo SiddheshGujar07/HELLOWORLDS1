a29205ffcacac194ae015bc1712da6e804af3783
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-12 22:46:53 +0530

    sdsdsdsdssdsdsdsdaassssddddfdfAA

---
+++ src/counts.java
@@ -0,0 +1,18 @@
+public class counts {
+
+    public static void main(String[] args) {
+
+        int n=18923;
+        int count=0;
+        while (n!=0)
+        {
+            count++;
+            n=n/10;
+
+        }
+        System.out.println(count);
+
+    }
+
+
+}
