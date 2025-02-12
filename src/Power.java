f1dcd91bd71c30c59fee7425c8b3ae9f77f812e4
Siddhesh <gujarsiddhesh603@gmail.com>
2025-02-12 22:46:35 +0530

    sdsdsdsdssdsdsdsdaassssddddfdfA

---
+++ src/Power.java
@@ -0,0 +1,20 @@
+public class Power {
+
+
+    public static void main(String[] args) {
+
+        /// 2^5//
+        int n = 2;
+        int p = 5;
+        int product = 1;
+
+        for (int i = 1; i <= p; i++) {
+            int result = product * i;
+
+            System.out.println(result);
+        }
+
+
+    }
+}
+
