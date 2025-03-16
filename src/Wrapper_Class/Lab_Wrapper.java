ae921d253d29330c9c1dff36c6d9a3c517c9d036
Siddhesh <gujarsiddhesh603@gmail.com>
2025-03-16 19:36:41 +0530

    sss

---
+++ src/Wrapper_Class/Lab_Wrapper.java
@@ -0,0 +1,27 @@
+package Wrapper_Class;
+
+public class Lab_Wrapper {
+
+    public static void main(String[] args) {
+
+        int a=10;
+        Integer b=a;
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(b.intValue());
+
+        Integer a2=42;
+        int v=a2;
+        System.out.println(v);
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ src/Wrapper_Class/real_example.java
@@ -0,0 +1,30 @@
+package Wrapper_Class;
+
+public class real_example {
+
+    public static void main(String[] args) {
+
+        String num="10";
+        int aa=39;
+
+        // String -> Wrapper conversion
+        Integer a=Integer.parseInt(num);
+        // String to Primitive
+        int a3=Integer.parseInt(num);
+
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+
+        // a  <Integer.MAX_VALUE
+        Integer aaa = Integer.valueOf("10");
+        System.out.println(aa);
+
+
+
+
+    }
+
+
+
+
+}
